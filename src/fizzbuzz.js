0c54b2fcc9e09dd5097650f793ce3148d5db28d4
Malte <malte.stengard@gmail.com>
2024-09-23 10:14:40 +0200

    all green

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,9 +2,23 @@ const answer = []
 
 // Write your code below this line
 
+const list = Array.from({ length: 15 }, (_, i) => i + 1)
+console.log(list)
 
+list.forEach((x, index) => {
+  if (x % 3 === 0) {
+    list[index] = 'Fizz'
+  }
+  if (x % 5 === 0) {
+    list[index] = 'Buzz'
+  }
+  if (x % 5 === 0 && x % 3 === 0) {
+    list[index] = 'FizzBuzz'
+  }
+})
+console.log(list)
 
-
+answer.push(...list)
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
